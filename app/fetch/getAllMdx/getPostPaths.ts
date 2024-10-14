fa5bfa08fb933bc20c95584297dd4ea93309239a
fefdfea <cankyu6@gmail.com>
2024-10-15 06:04:45 +0900

    블로그 url 점검

--- app/fetch/getAllMdx/getPostPaths.ts
+++ app/fetch/getAllMdx/getPostPaths.ts
@@ -2,10 +2,11 @@ import { sync } from "glob";
 import path from "path";
 
 export const getPostPaths = (category?: string, slug?: string) => {
-  const BASE_PATH = "/app/post";
+  const BASE_PATH = "app/post";
   const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
   const folder = category || "**";
   const fileName = slug || "**";
   const paths: string[] = sync(`${POSTS_PATH}/${folder}/**/${fileName}.mdx`);
+  console.log(POSTS_PATH);
   return paths;
 };

9be3820abb0cafdb09bf9ce16e6cee4fffe4cf6e
fefdfea <cankyu6@gmail.com>
2024-10-26 14:46:04 +0900

    log

--- app/fetch/getAllMdx/getThumbnail.ts
+++ app/fetch/getAllMdx/getThumbnail.ts
@@ -3,8 +3,51 @@ import path from "path";
 import fsExtra from "fs-extra";
 
 export async function getThumbnail(postUrl: string) {
+  if (process.env.NODE_ENV === "development") {
+    develop(postUrl);
+  } else {
+    production(postUrl);
+  }
+}
+
+function develop(postUrl: string) {
   const fullPath = postUrl.split("/").slice(0, 4).join("/");
   const categorySlug = postUrl.split("/").slice(2, 4).join("/");
+  const mdxFolderThumbnail = glob.sync(`${fullPath}/thumbnail.*`)[0];
+  const thumbnailsFolderThumbnail = glob.sync(
+    `public/thumbnails/${categorySlug}/thumbnail.*`
+  )[0];
+  if (mdxFolderThumbnail) {
+    const pasteDir = `public/thumbnails/${categorySlug}/${path.basename(mdxFolderThumbnail)}`;
+    fsExtra.copy(mdxFolderThumbnail, pasteDir, (err) => {
+      if (err) console.error(err);
+      // 복사후 기존 폴더의 이미지는 삭제
+      fsExtra.remove(mdxFolderThumbnail, (err) => {
+        if (err) console.error(err);
+      });
+    });
+  }
+
+  const thumbnail = glob.sync(
+    `public/thumbnails/${categorySlug}/thumbnail.*`
+  )[0];
+  console.log("postUrl", postUrl);
+  console.log("fullPath", fullPath);
+  console.log("categorySlug", categorySlug);
+  console.log("mdxFolderThumbnail", mdxFolderThumbnail);
+  console.log("thumbnailsFolderThumbnail", thumbnailsFolderThumbnail);
+  console.log("thumbnail", thumbnail);
+  if (thumbnailsFolderThumbnail) {
+    return `/thumbnails/${categorySlug}/${path.basename(thumbnailsFolderThumbnail)}`;
+  } else {
+    // 썸네일이 없는 경우 기본 이미지 반환
+    return "/img/noThumbnail/noImages.png";
+  }
+}
+
+function production(postUrl: string) {
+  const fullPath = postUrl.split("/").slice(2, 4).join("/");
+  const categorySlug = postUrl.split("/").slice(4, 6).join("/");
   const mdxFolderThumbnail = glob.sync(`${fullPath}/thumbnail.*`)[0];
   const thumbnailsFolderThumbnail = glob.sync(
     `public/thumbnails/${categorySlug}/thumbnail.*`
